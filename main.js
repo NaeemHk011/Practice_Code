06eec2fa10d7d67550a097587c7bd45e87503ba6
NaeemHk011 <itxkhaskheli@gmail.com>
2024-03-03 23:11:25 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// // Class-1
+// // 1. Itroduction of typescript
+// // 2. installation process
+// // 3.  console.log("Naeem CodeHub")
+// // YOUR 1ST ASSIGNMENT
+// // PRINT MY CHANNEL NAME IN CONSOLE
+var BATA = {
+    shoes: 9,
+};
+var SERVICE = {
+    shoes: 8
+};
+BATA = SERVICE;
+var ball = { diameter: 10 };
+var sphere = { diameter: 20 };
+sphere = ball;
+//   ball = sphere;
+console.log(sphere);
---
+++ main.ts
@@ -0,0 +1,973 @@
+// // Class-1
+// // 1. Itroduction of typescript
+// // 2. installation process
+// // 3.  console.log("Naeem CodeHub")
+// // YOUR 1ST ASSIGNMENT
+// // PRINT MY CHANNEL NAME IN CONSOLE
+
+
+
+// // Class.2
+// // 1. Variables...
+// // 2. word vs keyword...
+// // 3. data types...
+
+// // 1. Variables...
+// // code k anadar ksii data ko store  krne k liye ham variable ka use krty
+
+// // keyword... koi bhy aesa word jiska ts me koi mtlb ho woh keyword...
+
+// //  word... koi bhy aesa word jiska ts men koi mtlb na ho woh word he...
+// // boy girl,men, women , myname , naeem , ccap
+
+// // 3. data types...
+// // string.... koi bhy aesa data jo text me ho usko ham string khety hen....
+// // number: koi bhy aesa data jo numbers me ho use ham numbers data type
+
+// // let myName:string = "Naeem CodeHub";
+// // let age: number = 25;
+
+
+// // console.log(age);
+
+// // YOUR 2ND ASSIGNMENT
+// // STORE YOUR NAME IN A VARIABLE AND PRINT IT
+
+
+// // CLASS-3
+
+// // let Type:boolean = true
+// // console.log(Type)
+
+// // Personal Message: Store a person’s name in a variable, and print a message to that person.
+// //  Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+// // let myName:string = "Talib Hussain"
+
+// // console.log("Hello "+ myName + " would you like to learn some Typescript today?")
+
+
+// // VARIABLES THERE ARE THREE TYPES OF VARIABLES...
+// // 1. var
+// // 2.let
+// // 3.const
+
+// // var myName: string = "Naeem Hussain"
+// // var myName = "Talib Hussain"
+// // console.log(myName);
+
+// // var num:number = 22;
+// // var num:number = 10;
+// // var num:number = 14;
+// // var num:number = 14;
+
+// // num = 25;
+
+// // var ko ham ek name se bar bar bna bhy skty hen or br br uski value bhy change kr skty hen...
+
+// // let num:number = 22;
+// // // let num:number
+// // num = 10;
+
+// // const num:number = 50;
+
+// // OPERATORS
+// // 1. ARITHMETIC OPERATOR
+// // 2. ASSIGNMENT OPERATOR
+// // 3. COMPARISION OPERATOR
+
+// //  addition ARITHMETIC OPERATOR
+// // substraction ARITHMETIC OPERATOR
+// // div ARITHMETIC OPERATOR
+// // multipilication ARITHMETIC OPERATOR
+
+
+// // + addition
+// // let num1:number = 10;
+// // let num2:number = 5;
+// // console.log(num1 + num2);
+
+// // substraction
+// // let num1:number = 10;
+// // let num2:number = 5;
+// // console.log(num1 / num2);
+
+// // let firstName:string = "Naeem Hussain"
+// // console.log(firstName)
+
+// // ctrl+~
+
+// // ASSIGNMENT-3
+
+// // 2. Personal Message: Store  MY NAME  in a variable, and print a message to me.
+
+// // class -4
+// // Assignment operators
+
+// // let x = 20;
+// // console.log(x);
+
+// //  Assignment operators
+
+// // let a = 10;
+// // let b = 20
+// // // let c = b + a;
+
+// // console.log(b + a);
+
+
+// // COMPARISION OPERATORS
+
+// // 1.
+// // let y = 25;
+// // let x = "10"
+
+// // console.log(x == 10)
+
+// // 2.
+// // let A = 25;
+// // console.log(A === 25)
+
+// // 3
+// // 20 > 10?
+
+// // let X = 25;
+
+// // 4.
+// // console.log(X < 20)
+
+// // let X = 25;
+
+// // console.log(X > 20)
+
+
+// // let D = 20;
+
+// // console.log(D >= 20)
+
+// // 6.
+
+// //  let D = 20;
+
+// // console.log(D <= 20)
+
+// // let Y = 10;
+
+// // console.log(Y != 8)
+
+
+// // IF ELSE STATEMENT
+// // let student = "9th"
+
+// // if (student == "9th") {
+// //     console.log("Get in")
+// // }else{
+// //     console.log("Get Out")
+// // }
+
+// // if (){}else{}
+
+// // const age1:number = 15;
+
+// // if(age1 >= 18){
+// //     console.log("You are eligible for party")
+// // }else{
+// //     console.log("You are not eligible for party")
+// // }
+
+// // let age: number = 17
+
+// // if (age >= 18) {
+// //     console.log("You can drive")
+// // }
+// // else {
+// //     console.log("You cant drive")
+// // }
+
+// // LOGICAL OPERATORS
+
+// // 1- LOGICAL AND OPERATOR      &&
+
+
+// // let fname:string = "Naeem"
+// // let age= 18
+
+// // if(fname === "Naeem" && age === 17) {
+// // console.log("Login Succesfuly")
+// // }else{
+// //     console.log("Login failed")
+// // }
+
+// // const userName = "NaeemCodeHub"
+// // const Password = 786786
+
+// // if (userName === "NaeemCodeHub" && Password === 786786) {
+// //     console.log("Login Succesfuly")
+// // } else {
+// //     console.log("Login failed")
+// // }
+
+// // let percentage: number = 39
+
+// // if(percentage >= 80 && percentage <=100){
+// //     console.log("congratulations your grade is 'A+'")
+// // }else if(percentage >= 70 && percentage <=79){
+// //     console.log("congratulations your grade is 'A'")
+// // }else if(percentage >= 60 && percentage <=69){
+// //     console.log("congratulations your grade is 'B'")
+// // }else if(percentage >= 50 && percentage <=59){
+// //     console.log("congratulations your grade is 'C'")
+// // }else if(percentage >= 40 && percentage <=49){
+// //     console.log("conratulations your grade is 'D'")
+// // }else{
+// //     console.log("Sorry You are Fail")
+// // }
+
+// // Logical Or opertaor
+
+
+
+// // let age = 26;
+// // if(age >= 18 || age <= 25){
+// //     console.log("hello world")
+// // }else{
+// //     console.log("hello")
+
+// // let email = "Naeem@gmail.com";
+// // let Password = 786123
+
+// // if(email == "Naeem@gmail.com" || Password == 786100 ){
+// //     console.log("Welcome to facebook")
+// // }else{
+// //     console.log("Login Failed")
+// // }
+
+// // Conditional ternary operator
+// // let age = 21;
+// // let result = (age >= 18)? "You can drive" : "you can't drive"
+// // console.log(result)
+
+// // function sum(para1:string){
+// //     console.log(para1)
+
+// // }
+
+// // sum("Welcome tou website our Naeem")
+
+// // let myName = "naeem";
+// // // for UpperCase
+// // console.log(myName.toUpperCase())
+// // // for lower case
+// // console.log(myName.toLowerCase())
+
+
+// // function name(){}
+// // 1-koi bhy code agar hamne abhy nh chlana bad me chlana he jab ham use call kren  tou use ham function me rkhty hen
+
+// // 2- koi bhy aesa code jo hamne bar bar reuse krna he usko ham function k andar rakh dete hen
+
+// // 3- jab ham chahty hen k kisi code ko bar bar reuse kren lakin har bar wo hame difrnt value de tou ham use function me likhty
+
+// // function with parameters
+// // function myName(){
+// //     let myName:string = "Naeem";
+// //     let MyAge = 22;
+// //     console.log(`My Name is: ${myName} and my age is ${MyAge}`)
+// // }
+// // myName()
+// // function student(myName:string){
+// //     console.log(`My Name is: ${myName} `)
+// // }
+
+// // student("Naeem")
+// // student("malik")
+// // student("Hussain")
+// // student("Huss1")
+// // student("Huss12")
+// // student("Huss13")
+// // student("Huss14")
+// // student("Huss15")
+// // student("Huss16")
+// // student("Huss17")
+
+// // function sum(num1:number,num2:number){
+// //     console.log(num1*num2)
+// // }
+
+// // sum(5,3)
+// // sum(10,12)
+// // sum(15,15)
+
+// // MR Malik Result
+// // Phyics = 40
+// // Math = 50
+// // Chemistry = 60
+// // English = 60
+
+// // FUNCTION WITH PARAMETERS
+// // function result(Phyics:number,Math:number,Chemistry:number,English:number ){
+// //     console.log(Phyics+Math+Chemistry+English)
+// // }
+
+
+
+// // result(40,50,60,60);
+
+// // 3 students ka data banana he
+// // name?
+// // age?
+// // roll?
+// // let a:string = "Naeem"
+// // console.log(a)
+
+// // default
+
+
+
+// // function myName(a: string , b: string ="Hello") {
+// //     console.log(a+b)
+// // }
+// // myName("Naeem"," Hussain")
+
+
+
+
+
+
+// // function sum(physic: string, Chemistry: string, PhyPractiocal: string = "15 ", ChemPracticsl: string = "15") {
+// //     console.log(physic + Chemistry + PhyPractiocal + ChemPracticsl)
+// // }
+
+// // sum("60 ", "55 ", "13 ", "14")
+
+// // ARRAY
+// // Collection of Vlaue
+// // let pakistanTeam = ["Babar Azam","Fakhar zaman","Shadab Khan","Muhammad Rizwan"];
+// // console.log(pakistanTeam)
+
+// // let Marks:number[] = [55,489,4984]
+// // console.log(Marks)
+// // Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+// // 0               1               2           3       4
+// // let PakTeam = ["Babar Azam", "Muhammad Rizwan", "Shaheen", "Shadab", "Hassan"]
+// // // POP METHOD
+// // PakTeam.pop()
+
+
+// // let sub = ["physics", "chemistry", "english", "Urdu"]
+
+// // let Newsub = sub.pop()
+
+// // console.log(Newsub);
+
+// // let PakTeam = ["Babar Azam", "Muhammad Rizwan", "Shaheen", "Shadab"]
+// // // PUSH METHOD
+
+// // PakTeam.push("M Amir")
+// // console.log(PakTeam)
+
+
+
+// // let sub = ["physics", "chemistry", "english", "Urdu"]
+
+// // sub.push("Math")
+
+// // console.log(sub)
+
+// // let PakTeam = ["Babar Azam", "Muhammad Rizwan", "Shaheen", "Shadab"]
+// // PakTeam.shift()
+// // console.log(PakTeam)
+
+// // let sub = ["physics", "chemistry", "english", "Urdu"]
+// // let newSub = sub.shift()
+// // console.log(newSub)
+
+// // let PakTeam = ["Muhammad Rizwan", "Shaheen", "Shadab"]
+// // // UNSHIFT METHOD
+// // PakTeam.unshift("Shaheen")
+// // console.log(PakTeam)
+
+// // let sub = ["physics", "chemistry", "english", "Urdu","physics", "chemistry", "english", "Urdu","physics", "chemistry", "english", "Urdu","physics", "chemistry", "english", "Urdu"]
+
+// // let NewSub= sub.push("Math")
+// // console.log(NewSub)
+// // 0               1            2        3         4
+// // let PakTeam = ["Muhammad Rizwan", "Shaheen", "Shadab", "Babar", "Fakhar"]
+// // // SLICE METHOD
+// // PakTeam.slice(0, 2)
+
+// // console.log(PakTeam)
+
+// // SLICE METHOD
+
+// // Origional Array
+// // let PakTeam = ["Muhammad Rizwan", "Shaheen", "Shadab", "Babar", "Fakhar"]
+// // let newTeam = PakTeam.slice(1, 4)
+// // console.log(newTeam)
+
+// function myName(callback: any) {
+// callback()
+// }
+
+// function myTeam() {
+//     console.log("hello bilal")
+// }
+
+// myName(myTeam)
+
+//
+// Array Slice method ....
+
+// let OrigionalArray = ["Muhammad Rizwan", "Shaheen", "Shadab", "Babar", "Fakhar"]
+
+// let newArray = OrigionalArray.slice(0,3)
+// console.log(newArray)
+// SPLICE METHOD
+// let array = ["Naeem", "Malik", "Ahmed", "Sajjad", "Irfan"]
+// array.splice(1, 3,"Talib","Touseef","Kamal")
+// console.log(array)
+
+// // LOOP
+// for(let i = 1; i < 6 ; i++ ){
+
+//     console.log(i)
+
+// }
+
+// let i = 1;
+// i < 6;
+// i+1;
+
+// for(let i = 1;i <= 50; i++){
+//     console.log("Naeem CodeHub")
+// // }
+
+// for(let i = 1; i <= 10; i++){
+//     let numb = 2 ;
+//     let Answer = 2*i
+//     console.log(numb+" X "+ i+ " = "+ Answer)
+
+// // }
+// let arr = ["babar", "azam", "rizwan", "shadab", "shaheen", "dhani", "fakhar", "hassan"]
+// console.log(arr[])
+
+
+// for (let i = 0; i <= 5; i++) {
+//     console.log("X")
+//     for (let j = 1; j <= 3; j++) {
+
+//         console.log("*")
+//     }
+// }
+
+// let arr = ["ahmed", "khan", "babar"]
+// let array = ["Rizwan", "shadab", "fakhar"]
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(arr[i])
+
+//     for (let j = 0; j < 3; j++) {
+//         console.log(array[j])
+//     }
+// }
+
+
+// function table(rows: number, col: number) {
+
+//     for (let i = 1; i <= rows; i++) {
+//         for (let j = 1; j <= col; j++) {
+//             let result = i * j
+//             console.log(`${i} X ${j} = ${result}`)
+
+
+//         }
+//         console.log("\n")
+//     }
+
+// }
+
+// table(10, 10)
+
+
+
+// console.log("Naeem")
+// console.log("CodeHub")
+// console.log(1)
+// console.log(2)
+
+// process.stdout.write("Naeem")
+// // process.stdout.write("CodeHub")
+// process.stdout.write("naeem")
+// process.stdout.write("codehub")
+
+
+// function Height(height: number) {
+
+//     for (let i = 1; i <= height; i++) {
+
+//         for (let j = 1; j <= height - i; j++) {
+//             process.stdout.write(" ")
+//         }
+//         for (let k = 1; k <= 2 * i - 1; k++) {
+//             process.stdout.write("*")
+//         }
+//         console.log()
+
+//     }
+
+// }
+
+// Height(40)
+
+
+
+// SORT METHOD
+
+// let fruits = ["banana","apple","orange","grape"];
+
+// fruits.sort()
+// console.log(fruits)
+
+// let arr = [ 'A', 'B', 'C', 'D', 'E', 'F' ]
+// arr.reverse()
+// console.log(arr)
+// OBJECT
+// let Pakteam = ["babar","Rizwan","Shadab"]
+// console.log(Pakteam[0])
+
+// let obj = {
+//     name: "babar",
+//  age: 24,
+//  salary:20000,
+//  No: 26
+// }
+// console.log(obj)
+
+// let Pakteam: string[] = ["babar", "Rizwan", "Shadab"]
+
+// type Std = { name: string, age: number, RollNo: number }
+
+
+
+// let student: Std = {
+//     name: "kashan",
+//     age: 17,
+//     RollNo: 28372
+// }
+// let student1: Std = {
+//     name: "kashan",
+//     age: 17,
+//     RollNo: 28372
+// }
+// let student2: Std = {
+//     name: "kashan",
+//     age: 17,
+//     RollNo: 28372
+// }
+
+
+// NORMAL Function 
+// function Sum(a: number, b: number): number {
+//     return a + b
+// }
+
+// let  a = Sum(10, 10)
+// console.log(a)
+
+// ARROW FUNCTION 
+// const Sum = (a, b) => {
+//     let result = a + b
+//     return result
+
+// }
+// console.log(Sum(12, 6))
+
+// Union Types  
+// let Id:string | number | Boolean = "naeem"
+// Id = 49394
+// Id = true
+
+// let Sum = (sub1: number | string, sub2: number | string) => sub1 + sub2
+
+// let result = Sum(50, " fifty")
+// console.log(result)
+// SYNTAX ERROR 
+// lett message = "Hello World";//syntax error
+// console.log(message);
+
+// if[]{}
+
+// Type Error 
+// let message = "Hello World";
+// console.loger(message);
+
+// console.logere()
+
+
+// ASSIGNABILITY ERROR
+
+// let myName = "Naeem" 
+// myName = 5
+
+// let message = "Hello World";
+// message = 6;
+// console.log(message);
+
+
+// let Id = "b1-222"
+// Id = 222
+
+// D-STRUCTURE METHOD OF ARRAY & OBJECT
+
+// const arr = ["Naeem",22,"abc@gmail.com",38748]
+
+// const [name1,age,email] = arr 
+// console.log(name1,email)
+
+
+
+
+
+
+
+// const obj = {
+//     name1:"Naeem",
+//     age:22,
+//     email: "abc@gmail.com",
+//     RollNo: 38748374
+// }
+
+// const {name1,age,RollNo} = obj
+// console.log(age)
+
+
+
+// STRONG TYPING 
+// let nam:string = "Naeem"
+// nam = 857
+
+// let nam = "Naeem"
+
+// UNION TYPE 
+// let RollNo:number | string | boolean = 347393
+// RollNo = "Piaic36483"
+// RollNo = true
+
+// literal types 
+// let Academy: "CCAP" = "CCAP"
+// Academy = "pIAIC"
+
+// function myname(name:string, Academy: "CCAP"){
+//     console.log(name +" "+ Academy)
+// }
+// myname("Naeem", "CCAP")
+
+
+// OBJECT ALIAS 
+// type 
+// // interface 
+
+// interface stud {
+//     name: string,
+//     age:number,
+//     RollNo: number
+// }
+// interface stud {
+//     name: string,
+//     age:number,
+//     RollNo: number
+// }
+// interface stud {
+//     name: string,
+//     age:number,
+//     RollNo: number
+// }
+
+
+
+
+
+
+
+
+
+// let student1: stud = {
+//     name: "ahmed",
+//     age: 22,
+//     RollNo: 33667836
+// }
+
+// let student2 = {
+//     name: "Sajid",
+//     age: 22,
+//     RollNo: 33836
+// }
+
+// let student3  = {
+//     name: "Wajid",
+//     age: 12,
+//     RollNo: 333836
+// }
+
+// STRUTURAL TYPING 
+
+
+
+
+// interface Ball {
+//     diameter: number;
+// }
+
+// interface Sphere {
+//     diameter: number;
+// }
+
+//   let ball: Ball = { diameter: 10 };
+//   let sphere: Sphere = { diameter: 20 };
+
+// //   sphere = ball;
+//   ball = sphere;
+// console.log(ball)
+
+
+// interface bata{
+//     shoes:number
+//     daba:number
+// }
+
+// interface service{
+//     shoes:number
+// }
+
+// let BATA:bata = {
+//     shoes: 9,
+//     daba: 9
+
+// }
+
+// let SERVICE:service = {
+//     shoes: 8
+// }
+
+// BATA = SERVICE
+
+// SERVICE = BATA
+// / interface Ball {
+//         diameter: number;
+//     }
+
+// interface Sphere {
+//     diameter: number;
+// }
+
+//   let ball: Ball = { diameter: 10 };
+//   let sphere: Sphere = { diameter: 20 };
+
+// //   sphere = ball;
+//   ball = sphere;
+// console.log(ball)
+
+
+// interface Tube {
+//     diameter: number;
+//     length: number;
+//   }
+
+//   let tube: Tube = { diameter: 12, length: 3 };
+
+//   //tube = ball;//Error
+//   ball = tube;
+
+// interface blueprint{
+//     room:number,
+//     kitchen:number
+// }
+
+// let home:blueprint = {room: 2,kitchen:1}
+
+// home = {room:4,kitchen:2}
+
+// let student: std = { name: "Naeem", id: 8347 }
+
+// student = { name: "Ahmed", id: 849384 }
+
+// interface std { [x: string]: any, id: number }
+
+// // INDEX SIGNATURE 
+
+// student = { l_name: "Ahmed", id: 849384 }
+
+
+
+
+
+
+
+
+
+// let myType = { name: "Zia", id: 1 };
+
+// //Case 1
+// myType = { id: 2,  name: "Tom" };//Case 1: can only assign a type which has the the same properties
+// 									//Object literals can only have properties that exist in contextual type
+
+
+// //Case 2a
+// myType = { id: 2,  name_person: "Tom" };
+
+// var x: { id: number, [x: string]: any };//Note now 'x' can have any name, just that the property should be of type string
+
+// x = { id: 1, firstname: "Zia" };  
+
+
+
+
+// interface mytype{
+//     id:number,
+//     [x:string]:any
+// // }
+// myType = { id: 2,  name_person: "Tom" };
+
+// // myType = { id: 2,  name: "Tom", age: 22 };
+
+// // let myType = { name: "Zia", id: 1 };
+// interface types{
+//     id:number,
+//     [x:string]:any
+// }
+
+
+// let myType : types= { name: "Zia", id: 1 };
+
+// let myType3:types = { id: 2,  namel_person: "Tom" };
+
+
+
+// myType = myType3
+
+// myType3 = myType
+
+// let myType = { name: "Zia", id: 1 };
+// // myType = { id: 2,  name: "Tom" };
+// // myType = { id: 2,  name_person: "Tom" };
+// // myType = { id: 2,  name: "Tom", age: 22 };
+
+// var myType4 = { id: 2,  name: "Tom", age: 22 };
+
+// myType = myType4;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
